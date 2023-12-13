5cd678cac92bc3983002a53d7d055cc7dce9e3ef
PramisGrg <gurungpramis361@gmail.com>
2023-12-13 23:33:51 +0545

    Readme

--- src/components/Todoform.js
+++ src/components/Todoform.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 const Todoform = () => {
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(todo);
+  };
+
   return (
     <div>
-      <div className="todo-container">
+      <div className="todo-container" onSubmit={handleSubmit}>
         <form className="todo-form">
           <input
             type="text"
             className="todo-input"
             placeholder="What is your task today ?"
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
           ></input>
           <button type="submit" className="todo-btn">
             Add Task
